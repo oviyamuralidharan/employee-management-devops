2fc0ad23c3558df4d47b4d554809d16478e737d0
Oviya_CM <oviyacmuralidharan@gmail.com>
2026-07-28 16:13:18 +0530

    Initial Employee Management System using Flask

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+employees = [
+    {
+        "id": 101,
+        "name": "John",
+        "department": "HR",
+        "email": "john@example.com"
+    },
+    {
+        "id": 102,
+        "name": "David",
+        "department": "IT",
+        "email": "david@example.com"
+    },
+    {
+        "id": 103,
+        "name": "Sarah",
+        "department": "Finance",
+        "email": "sarah@example.com"
+    }
+]
+
+@app.route("/")
+def home():
+    return render_template("index.html", employees=employees)
+
+if __name__ == "__main__":
+    app.run(debug=True)
